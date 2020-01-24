d267ccbeddfe0df4b5459e58f1f0cc9bdcd9a33a
George BV <egor.workroom@gmail.com>
2020-01-25 02:46:33 +0600

    Adds enviroment and release and helpers functions

--- config.go
+++ config.go
@@ -11,7 +11,62 @@ import (
 type Configuration struct {
 	Tags              map[string]string
 	DisableStacktrace bool
+	Enviroment        string
+	Release           string
 	Level             zapcore.Level
 	FlushTimeout      time.Duration
 	Hub               *sentry.Hub
+}
+
+type Option func(*Configuration)
+
+func Enviroment(env string) Option {
+	return func(opt *Configuration) {
+		opt.Enviroment = env
+	}
+}
+
+func Release(release string) Option {
+	return func(opt *Configuration) {
+		opt.Release = release
+	}
+}
+
+func AddTag(key, val string) Option {
+	return func(opt *Configuration) {
+		if opt.Tags == nil {
+			opt.Tags = make(map[string]string)
+		}
+		opt.Tags[key] = val
+	}
+}
+
+func DisableStacktrace() Option {
+	return func(opt *Configuration) {
+		opt.DisableStacktrace = true
+	}
+}
+
+func EnableStacktrace() Option {
+	return func(opt *Configuration) {
+		opt.DisableStacktrace = false
+	}
+}
+
+func ErrorLevel(level zapcore.Level) Option {
+	return func(opt *Configuration) {
+		opt.Level = level
+	}
+}
+
+func FlushTimeout(d time.Duration) Option {
+	return func(opt *Configuration) {
+		opt.FlushTimeout = d
+	}
+}
+
+func SentryHub(hub *sentry.Hub) Option {
+	return func(opt *Configuration) {
+		opt.Hub = hub
+	}
 }
